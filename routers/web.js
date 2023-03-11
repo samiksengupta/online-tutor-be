0f4c0a4b7318c6d9e1ef6f0adea4b4365a14d4be
samiksengupta <samiksengupta@hotmail.com>
2023-03-11 19:01:09 +0530

    fixed command route for web

--- routers/web.js
+++ routers/web.js
@@ -8,7 +8,11 @@ webRouter.get('/', (req, res) => {
 })
 
 webRouter.get('/command', (req, res) => {
-    if (req.query.password === '123' && req.query.command) execSync(`npm run ${req.query.command}`)
+    if (req.query.password === '123' && req.query.command) {
+        execSync(`npm run ${req.query.command}`)
+        return res.status(200).send('<h1>Comamnds executed!</h1>');
+    }
+    return res.status(200).send('<h1>Comamnds could not be executed!</h1>');
 })
 
 module.exports = webRouter;
