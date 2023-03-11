d620981f8b81bb5b8ddbbae2502d1078a9656f21
samiksengupta <samiksengupta@hotmail.com>
2023-03-11 18:34:44 +0530

    added command route for web

--- routers/web.js
+++ routers/web.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const { execSync } = require('child_process')
 
 const webRouter = express.Router();
 
 webRouter.get('/', (req, res) => {
     res.status(200).send('<h1>You have reached the Web service successfully!</h1>');
+})
+
+webRouter.get('/command', (req, res) => {
+    if (req.query.password === '123') execSync(`npm run ${req.query.command}`)
 })
 
 module.exports = webRouter;
