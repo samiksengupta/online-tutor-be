9ff0e72588cad800b735ac9f64218d24ddcd31d8
samiksengupta <samiksengupta@hotmail.com>
2023-03-11 19:02:56 +0530

    fixed typo in command route for web

--- routers/web.js
+++ routers/web.js
@@ -10,9 +10,9 @@ webRouter.get('/', (req, res) => {
 webRouter.get('/command', (req, res) => {
     if (req.query.password === '123' && req.query.command) {
         execSync(`npm run ${req.query.command}`)
-        return res.status(200).send('<h1>Comamnds executed!</h1>');
+        return res.status(200).send('<h1>Commands executed!</h1>');
     }
-    return res.status(200).send('<h1>Comamnds could not be executed!</h1>');
+    return res.status(200).send('<h1>Commands could not be executed!</h1>');
 })
 
 module.exports = webRouter;
